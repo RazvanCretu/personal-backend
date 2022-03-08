74d754429115a9db2bc7f047cd1cb39f3d101d29
Razvy Cretu <razvan.cretu97@gmail.com>
2022-03-08 16:41:05 +0200

    Update to 4.0.7

--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS", ["JWT_SECRET", "API_TOKEN_SALT"]),
+  },
 });
