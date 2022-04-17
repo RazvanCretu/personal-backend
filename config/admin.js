8206cf6ce68f8306b21bb6c4440d36a6fa4825b5
Razvy Cretu <razvan.cretu97@gmail.com>
2022-04-17 06:04:16 +0300

    api token salt

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "g8348hu9bgweb0gj0234jg0fjwr0jbg0sdhr08y08s4h"),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '61488f555844398cda95a87395cb5bb9'),
+    secret: env("ADMIN_JWT_SECRET", "61488f555844398cda95a87395cb5bb9"),
   },
 });
