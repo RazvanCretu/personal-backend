e80995fa17f1852c67a22316fea2232a5ac44826
Razvy Cretu <razvan.cretu97@gmail.com>
2022-04-17 06:53:41 +0300

    admin jwt

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   apiToken: { salt: env("API_TOKEN_SALT") },
   auth: {
-    secret: env("ADMIN_JWT_SECRET", "61488f555844398cda95a87395cb5bb9"),
+    secret: env("ADMIN_JWT_SECRET"),
   },
 });
