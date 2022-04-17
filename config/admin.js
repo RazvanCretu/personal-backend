244e1ef09ece2b8b74aede47f69897d6a3a20eb5
Razvy Cretu <razvan.cretu97@gmail.com>
2022-04-17 06:33:16 +0300

    reverse

--- config/admin.js
+++ config/admin.js
@@ -1,7 +1,4 @@
 module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "g8348hu9bgweb0gj0234jg0fjwr0jbg0sdhr08y08s4h"),
-  },
   auth: {
     secret: env("ADMIN_JWT_SECRET", "61488f555844398cda95a87395cb5bb9"),
   },
